c6043c417a9c53ce21c3af19ccf890483810c058
David Geiss <dave.geiss@gmail.com>
2017-06-12 11:29:05 -0400

    cat, head, tail done

--- bash.js
+++ bash.js
@@ -6,15 +6,32 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-
+  let cmdArr = cmd.split(" ");
   //process.stdout.write('You typed: ' + cmd);
-  if (cmd === "pwd") {
-    commands.pwd();
-  } else if (cmd === "date") {
-    commands.date();
-  } else if (cmd == "ls") {
-    commands.ls();
-  } else {
-    commands.prompt();
+  switch(cmdArr[0])
+  {
+    case "pwd":
+      commands.pwd();
+      break;
+    case "date":
+      commands.date();
+      break;
+    case "ls":
+      commands.ls();
+      break;
+    case "echo":
+      commands.echo(cmdArr.slice(1));
+      break;
+    case "cat":
+      commands.cat(cmdArr[1]);
+      break;
+    case "head":
+      commands.head(cmdArr[1]);
+      break;
+    case "tail":
+      commands.tail(cmdArr[1]);
+      break;
+    default:
+      commands.prompt();
   }
 });
--- commands.js
+++ commands.js
@@ -1,21 +1,53 @@
 var fs = require('fs');
 
-exports.prompt = function() {
+exports.prompt = function(filename) {
     process.stdout.write('\nprompt > ');
-}
+};
 
-exports.pwd = function() {
+exports.pwd = function(filename) {
     process.stdout.write(process.cwd());
     this.prompt();
-}
+};
 
-exports.date = function() {
+exports.date = function(filename) {
     process.stdout.write(new Date().toString());
     this.prompt();
-}
+};
 
-exports.ls = function() {
-    var files = fs.readdirSync(process.cwd());
+exports.ls = function(filename) {
+    fs.readdir('.', (err, data) => {
+        data.forEach(file => {
+            if(file[0] !== '.')
+                process.stdout.write(file + '\n');
+        });
+        this.prompt();
+    });
+};
 
-    files.forEach(file => process.stdout.write(file + "\n"));
-}
+exports.echo = function(filename) {
+    process.stdout.write(filename.join(" "));
+    this.prompt();
+};
+
+exports.cat = function(filename) {
+    fs.readFile(filename, 'utf-8', (err, data) =>{
+        process.stdout.write(data);
+        this.prompt();
+    });
+};
+
+exports.head = function(filename) {
+    fs.readFile(filename, 'utf-8', (err, data) => {
+        let split = data.split('\n');
+        process.stdout.write(split.slice(0, 5).join('\n'));
+        this.prompt();
+    });
+};
+
+exports.tail = function(filename) {
+    fs.readFile(filename, 'utf-8', (err, data) => {
+        let split = data.split('\n');
+        process.stdout.write(split.slice(-5).join('\n'));
+        this.prompt();
+    });
+};
